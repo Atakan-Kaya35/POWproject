603f153fac8742a688ae65d7d85eac86dd24f11a
Atakan <atakanka350@gmail.com>
2024-04-16 14:31:41 +0300

    SQL Starting Point

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class App extends Application {
+    @FXML
+    private Button loginButton;
+
+    public void start(Stage primaryStage) throws IOException{
+
+        //EventListener
+        /* loginButton.setOnAction(e -> {
+            HomePage.show(primaryStage);
+        }); */
+
+        FXMLLoader fxml = new FXMLLoader(App.class.getResource("Merhaba.fxml"));
+        Scene scene1 = new Scene(fxml.load(),1080,720);
+        primaryStage.setScene(scene1);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
