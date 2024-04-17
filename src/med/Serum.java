91dc1542c365de0e13f6ae07265a44b8b3f76419
Burkay Tunçtürk <burkay.tuncturk@gmail.com>
2024-04-17 23:08:10 +0300

    ss

--- src/med/Serum.java
+++
@@ -1,7 +0,0 @@
-package med;
-public class Serum extends Medicine{
-    public Serum(){
-        super("Serum", "A serum to rehydrate and replenish your electrolytes.");
-        setCyclesOfTaking(1);
-    }
-}
